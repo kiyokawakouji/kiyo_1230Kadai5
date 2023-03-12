de9c1dc338af4ffb31be5a9daf3bebe1abb233dc
kiyokawakouji <kohji12k@gmail.com>
2023-03-13 00:29:40 +0900

    create project

---
+++ kiyo_1230Kadai5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kiyo_1230Kadai5
+//
+//  Created by 清川光司 on R 5/03/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
