c5252102d310a86f5ed0d167eeabcbc86d3a14b7
kiyokawakouji <kohji12k@gmail.com>
2023-03-13 00:47:07 +0900

    TODOコメント追加

--- kiyo_1230Kadai5/ViewController.swift
+++ kiyo_1230Kadai5/ViewController.swift
@@ -14,6 +14,22 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    // TODO: UI部品の設置 → textField x2、label x2、button
+
+    // TODO: 割り算のロジックを作る
+    // TODO: leftTextFieldが空の場合アラートダイアログAを表示する
+    // TODO: アラートダイアログA「title:課題5、subtitle:割られる数を入力して下さい、button:OK」
+
+
+
+    // TODO: rightTextFieldが空の場合アラートダイアログBを表示する
+    // TODO: アラートダイアログB「title:課題5、subtitle:割る数を入力して下さい、button:OK」
+
+
+
+    // TODO: leftTextFieldが「0」の場合アラートダイアログCを表示する
+    // TODO: アラートダイアログC「title:課題5、subtitle:割る数には0を入力しないで下さい、button:OK」
+
 
 }
 
