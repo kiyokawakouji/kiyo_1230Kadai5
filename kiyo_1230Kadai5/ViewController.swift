15a39d6e6cab26099d21370a7a068834224b5ce2
kiyokawakouji <kohji12k@gmail.com>
2023-03-21 23:13:54 +0900

    alertの共通化

--- kiyo_1230Kadai5/ViewController.swift
+++ kiyo_1230Kadai5/ViewController.swift
@@ -13,23 +13,15 @@ class ViewController: UIViewController {
     @IBOutlet private weak var numeratorNumber: UITextField!
     @IBOutlet private weak var denominatorNumber: UITextField!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
     @IBAction private func calculationButton(_ sender: Any) {
 
         if numeratorNumber.text == "" {
-        let alertA = UIAlertController(title: "課題5", message: "割られる数を入力して下さい", preferredStyle: .alert)
-            alertA.addAction(UIAlertAction(title: "OK", style: .default))
-            self.present(alertA, animated: true, completion: nil)
+            commonAlert(alertMessage: "割られる数を入力して下さい")
             return
         }
 
         if denominatorNumber.text == "" {
-        let alertB = UIAlertController(title: "課題5", message: "割る数を入力して下さい", preferredStyle: .alert)
-            alertB.addAction(UIAlertAction(title: "OK", style: .default))
-            self.present(alertB, animated: true, completion: nil)
+            commonAlert(alertMessage: "割る数を入力して下さい")
             return
         }
 
@@ -37,12 +29,16 @@ class ViewController: UIViewController {
         let inputDenominatorNumber = Int(denominatorNumber.text ?? "") ?? 0
 
         if inputDenominatorNumber == 0 {
-            let alertC = UIAlertController(title: "課題5", message: "割る数には0を入力しないで下さい", preferredStyle: .alert)
-            alertC.addAction(UIAlertAction(title: "OK", style: .default))
-            self.present(alertC, animated: true, completion: nil)
+            commonAlert(alertMessage: "割る数には0を入力しないで下さい")
         }
 
         let result = Double(inputNumeratorNumber) / Double(inputDenominatorNumber)
-            resultLabel.text = result.description
+        resultLabel.text = result.description
+    }
+
+    private func commonAlert(alertMessage: String) {
+        let alert = UIAlertController(title: "課題5", message: alertMessage, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        self.present(alert, animated: true, completion: nil)
     }
 }
